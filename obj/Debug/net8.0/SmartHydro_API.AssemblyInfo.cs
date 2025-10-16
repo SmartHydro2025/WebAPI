4e3f4313ff9356b094ca96746505121721da1df3
jcvheerden <ST10316473@vcconnect.edu.za>
2025-10-16 18:50:35 +0200

    Docker-compose.yml

--- obj/Debug/net8.0/SmartHydro_API.AssemblyInfo.cs
+++ obj/Debug/net8.0/SmartHydro_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartHydro_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01c5295614e5eacf584aa1ed8f4bc095c4aaed50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1a587cd049f4f66cb93cff305b5af5809d5433d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartHydro_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartHydro_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Release/net8.0/SmartHydro_API.AssemblyInfo.cs
+++ obj/Release/net8.0/SmartHydro_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartHydro_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e858abe517a0afb38cbb339663f23b1a20fdd42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1a587cd049f4f66cb93cff305b5af5809d5433d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartHydro_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartHydro_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
