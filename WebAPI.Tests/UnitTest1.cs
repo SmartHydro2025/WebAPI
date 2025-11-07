13303c8ffecb091b3a64975afe14d1959527eb14
Keely-Ann <ST10085428@vcconnect.edu.za>
2025-11-07 22:45:05 +0200

    Added Unit Testing Methods
    GetTents, GetSensorReadings and GetTempAIEvent

--- WebAPI.Tests/UnitTest1.cs
+++ WebAPI.Tests/UnitTest1.cs
@@ -1,13 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using SmartHydro_API.Database;
+using SmartHydro_API.LiveCache;
 using Xunit;
 
 namespace WebAPI.Tests;
 
 public class UnitTest1
 {
+    private readonly SmartHydroDbContext _dbContext;
+    private readonly LiveSensorCache _cache;
+    private readonly AIEventCache _AIcache;
+
     [Fact]
-    public void BuildPass()
+    public void GetTents()
     {
-        Assert.True(true);
+        var tentDetails = _dbContext.TentInformation.ToList();
+        Assert.NotNull(tentDetails);
+    }
+
+    [Fact]
+    public void GetSensorReadings()
+    {
+        string mac = "BD:1D:B6:7C:E5:A4";
+        var reading = _cache.GetLatest(mac);
+
+        Assert.NotNull(reading);
+    }
+
+    [Fact]
+    public void GetTempAIEvent()
+    {
+        string mac = "BD:1D:B6:7C:E5:A4";
+        var aiEvent = _AIcache.GetLatest(mac);
+        Assert.NotNull(aiEvent.Message);
+
+        var dbEvent = _dbContext.AiEvents
+                    .Where(e => e.Mac == mac && e.Sensor.Equals("Temperature", StringComparison.OrdinalIgnoreCase))
+                    .OrderByDescending(e => e.ID) 
+                    .FirstOrDefault();
+        Assert.NotNull(dbEvent.Message);
+
     }
 }
-//test 
