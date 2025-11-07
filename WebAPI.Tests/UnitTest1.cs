e8faed150feb1ee368eb5edb96ff28886bd9ef98
Keely-Ann <ST10085428@vcconnect.edu.za>
2025-11-07 22:49:37 +0200

    Removed test methods

--- WebAPI.Tests/UnitTest1.cs
+++ WebAPI.Tests/UnitTest1.cs
@@ -18,27 +18,4 @@ public class UnitTest1
         Assert.NotNull(tentDetails);
     }
 
-    [Fact]
-    public void GetSensorReadings()
-    {
-        string mac = "BD:1D:B6:7C:E5:A4";
-        var reading = _cache.GetLatest(mac);
-
-        Assert.NotNull(reading);
-    }
-
-    [Fact]
-    public void GetTempAIEvent()
-    {
-        string mac = "BD:1D:B6:7C:E5:A4";
-        var aiEvent = _AIcache.GetLatest(mac);
-        Assert.NotNull(aiEvent.Message);
-
-        var dbEvent = _dbContext.AiEvents
-                    .Where(e => e.Mac == mac && e.Sensor.Equals("Temperature", StringComparison.OrdinalIgnoreCase))
-                    .OrderByDescending(e => e.ID) 
-                    .FirstOrDefault();
-        Assert.NotNull(dbEvent.Message);
-
-    }
 }
