339dfba3df3bfad220bff22c4f02e7b55db4f345
jcvheerden <ST10316473@vcconnect.edu.za>
2025-11-07 20:45:22 +0200

    Added an image controller

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartHydro_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImageController : Controller
+    {
+
+        private readonly IWebHostEnvironment _env;
+
+        public ImageController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Image(string name)
+        {
+
+            try
+            {
+                var imagePath = Path.Combine(_env.ContentRootPath, "images", name);
+
+                if (!System.IO.File.Exists(imagePath))
+                {
+                    return NotFound();
+                }
+
+                var imageBytes = System.IO.File.ReadAllBytes(imagePath);
+
+                //Stack Overflow. 2016
+                return File(imageBytes, "image/jpeg");
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+
+        }
+
+    }
+}
+
+
+/*
+ REFERENCES
+==================
+
+Stack Overflow. 2016. Can an ASP.NET MVC controller return an Image? [Online]. Available at:  https://stackoverflow.com/questions/186062/can-an-asp-net-mvc-controller-return-an-image
+ */
--- obj/Debug/net8.0/SmartHydro_API.AssemblyInfo.cs
+++ obj/Debug/net8.0/SmartHydro_API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartHydro_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb6e5e76e88d2343f82ae0091114c07a9fb54d2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1899b0d530f8d1c805cb32d6ea37db85b54125f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartHydro_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartHydro_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
